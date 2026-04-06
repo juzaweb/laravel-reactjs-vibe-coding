c3062e420001d42628f7bb158f0aba772329aaf4
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-04-06 13:59:39 +0700

    refactor: remove redundant header comment block from api routes file

--- modules/Api/routes/api.php
+++ modules/Api/routes/api.php
@@ -6,17 +6,6 @@ use Juzaweb\Modules\Api\Http\Controllers\API\ProfileController;
 use Juzaweb\Modules\Api\Http\Controllers\API\SettingController;
 use Juzaweb\Modules\Api\Http\Controllers\API\TranslationController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::get('settings', [SettingController::class, 'index']);
 Route::get('translations/{locale}', [TranslationController::class, 'index']);
 

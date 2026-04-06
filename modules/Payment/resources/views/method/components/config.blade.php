2670d4ae63a06359893ed2f8c454ddb16d547070
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-04-06 16:06:58 +0700

    refactor: remove legacy Blade view files and language resources from Payment and Subscription modules

--- modules/Payment/resources/views/method/components/config.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach($fields as $name => $label)
-    {{ Field::security($label, "config[{$name}]", ['value' => $config[$name] ?? null]) }}
-@endforeach
-
-@if($hasSandbox)
-    {{ Field::checkbox(__('Test Mode'), 'config[sandbox]', ['value' => $config['sandbox'] ?? false]) }}
-@endif
--- modules/Subscription/resources/views/method/components/config.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach($fields as $name => $label)
-    {{ Field::text($label, "config[{$name}]", ['value' => $config[$name] ?? null]) }}
-@endforeach
-
-@if($hasSandbox)
-    @foreach($fields as $name => $label)
-        {{ Field::text($label . ' (Sandbox)', "config[sandbox_{$name}]", ['value' => $config["sandbox_{$name}"] ?? null]) }}
-    @endforeach
-@endif

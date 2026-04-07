e40b8c100632d5886c1501a29e8045dfeb6b1309
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-04-07 10:29:08 +0700

    refactor: update MediaContract namespace and document API response standards in AGENTS.md

--- modules/Api/Http/Controllers/API/MediaController.php
+++ modules/Api/Http/Controllers/API/MediaController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Juzaweb\Modules\Api\Http\Requests\MediaBulkRequest;
 use Juzaweb\Modules\Api\Http\Requests\MediaRequest;
 use Juzaweb\Modules\Api\Http\Resources\MediaResource;
-use Juzaweb\Modules\Core\Contracts\MediaContract;
+use Juzaweb\Modules\Core\Contracts\Media as MediaContract;
 use Juzaweb\Modules\Core\Http\Controllers\APIController;
 use Juzaweb\Modules\Core\Models\Media;
 use OpenApi\Annotations as OA;

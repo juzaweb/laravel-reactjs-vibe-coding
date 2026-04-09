c0d7a0f8256e9a7c33a4c5ab3b917e5eda40168b
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-04-09 13:09:30 +0700

    refactor: remove the Installer module and its associated controllers, views, and tests

--- modules/Installer/resources/views/update/overview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('installer::layouts.master-update')
-
-@section('title', trans('installer::message.updater.welcome.title'))
-@section('container')
-    <p class="paragraph text-center">{{ trans_choice('installer::message.updater.overview.message', $numberOfUpdatesPending, ['number' => $numberOfUpdatesPending]) }}</p>
-    <div class="buttons">
-        <a href="{{ route('LaravelUpdater::database') }}" class="button">{{ trans('installer::message.updater.overview.install_updates') }}</a>
-    </div>
-@stop

2a47bd0df0a6afe2d75a5bd07ee7cc2db3e79a90
Đặng Thế Anh <anh.dt@drdigital.vn>
2026-04-10 15:39:09 +0700

    refactor: move ads API routes into authenticated middleware group and remove redundant banner-ads route

--- modules/AdsManagement/routes/api.php
+++ modules/AdsManagement/routes/api.php
@@ -4,20 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Juzaweb\Modules\AdsManagement\Http\Controllers\BannerAdsController;
 use Juzaweb\Modules\AdsManagement\Http\Controllers\VideoAdsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::api('banner-ads', BannerAdsController::class);
-
 Route::prefix('app')->group(function () {
     Route::get('banner-ads', [Juzaweb\Modules\AdsManagement\Http\Controllers\App\BannerAdsController::class, 'index']);
 });
-Route::api('video-ads', VideoAdsController::class);
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::api('video-ads', VideoAdsController::class);
+    Route::api('banner-ads', BannerAdsController::class);
+});
